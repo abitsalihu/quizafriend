982df322e81ae8e309683cd0dd6f955197e76742
Abit Salihu <abiti.salihu@gmail.com>
2022-09-23 11:06:04 +0200

    add audio manipulation

--- script.js
+++ script.js
@@ -5,14 +5,15 @@ const buttons = document.querySelector(".buttons");
 const createQuizBTN = document.querySelector(".creating");
 const takeQuizBTN = document.querySelector(".taking");
 const body = document.querySelector("body");
+const btnClick = new Audio("audio/button.wav");
+const swooshAway = new Audio("audio/whoosh.wav");
+const swoopIn = new Audio("audio/transition_up.wav");
 
 const displayNone = function () {
   title.style.display = "none";
   createQuizBTN.style.display = "none";
   takeQuizBTN.style.display = "none";
 };
-
-const backgroundChange = function () {};
 
 buttons.addEventListener("click", function (e) {
   if (e.target.classList.contains("buttons__btn")) {
@@ -25,9 +26,16 @@ buttons.addEventListener("click", function (e) {
     setTimeout(displayNone, 1500);
     body.classList.add(nextBackground);
     nextPage = document.querySelector(`.${nextPage}`);
+    btnClick.play();
+    setTimeout(function () {
+      swooshAway.play();
+    }, 175);
     setTimeout(function () {
       nextPage.style.display = "flex";
     }, 1500);
+    setTimeout(function () {
+      swoopIn.play();
+    }, 1200);
   }
   //   title.style.backgroundColor = "red";
 });
