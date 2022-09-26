0f01e090005c9d1275176f429d733b945f994dc5
Abit Salihu <abiti.salihu@gmail.com>
2022-09-26 14:47:36 +0200

    finish codingQuiz questions, and make page responsive to start a quiz

--- script.js
+++ script.js
@@ -18,16 +18,16 @@ const Quiz = function (
 //? coding quiz
 
 const codingQuestion10 = new Quiz(
-  "What property do we use to change font type",
-  ["font-size", "font-weight", "font-family", "text-transform"],
-  "c",
+  "What property do we use to create a website responsive?",
+  ["@animation", "@media", "@keyframes", "@break"],
+  "b",
   "none",
   true
 );
 
 const codingQuestion9 = new Quiz(
-  "What property do we use to change font type",
-  ["font-size", "font-weight", "font-family", "text-transform"],
+  "What tag do we use to create a link in HTML?",
+  ["<a>", "<img>", `href=""`, "<link>"],
   "c",
   codingQuestion10,
 
@@ -35,25 +35,25 @@ const codingQuestion9 = new Quiz(
 );
 
 const codingQuestion8 = new Quiz(
-  "What property do we use to change font type",
-  ["font-size", "font-weight", "font-family", "text-transform"],
-  "c",
+  "Which is the biggest title in HTML?",
+  ["h1", "h6", "h3", "h5"],
+  "a",
   codingQuestion9,
 
   false
 );
 
 const codingQuestion7 = new Quiz(
-  "What property do we use to change font type",
-  ["font-size", "font-weight", "font-family", "text-transform"],
-  "c",
+  "What property do we use to give space to the outside of a container?",
+  ["padding", "top", "left", "margin"],
+  "d",
   codingQuestion8,
 
   false
 );
 
 const codingQuestion6 = new Quiz(
-  "What property do we use to change font type",
+  "What property do we use to change font type?",
   ["font-size", "font-weight", "font-family", "text-transform"],
   "c",
   codingQuestion7,
@@ -62,7 +62,7 @@ const codingQuestion6 = new Quiz(
 );
 
 const codingQuestion5 = new Quiz(
-  "How do we position something on the UI so that it's always on the screen no matter where we are?",
+  "How do we position something on the UI so that it's always on the screen no matter where scroll to?",
   [
     "position:absolute;",
     "position:relative;",
@@ -83,14 +83,14 @@ const codingQuestion4 = new Quiz(
 );
 
 const codingQuestion3 = new Quiz(
-  "What doed HTML stand for?",
+  "What does HTML stand for?",
   [
-    "HyperText Markdown Language",
+    "HyperText Markup Language",
     "HyperText Markside language",
     "HyperText Markdown Language",
     "Cascading Style Sheet",
   ],
-  "c",
+  "a",
   codingQuestion4,
   false
 );
@@ -111,14 +111,29 @@ const codingQuestion1 = new Quiz(
   false
 );
 
-console.log(codingQuestion1);
-console.log(codingQuestion2);
+//! STARTING CODE FOR QUIZES
+
+let currentQuestion;
+let correctAnswer;
+let nextQuestion;
+let nextPage;
+
+let userChosenAnswer;
+
+//!
+
 //? Landing PAGE DOM
 const title = document.querySelector(".title");
 const buttons = document.querySelector(".buttons");
 const createQuizBTN = document.querySelector(".creating");
 const takeQuizBTN = document.querySelector(".taking");
 const body = document.querySelector("body");
+const chooseQuiz = document.querySelectorAll(".take-quiz-btn");
+const startQuiz = document.querySelector(".start");
+const answersBtnCon = document.querySelector(".start__answers");
+const allAnswers = document.querySelectorAll(".start__answers--btn");
+// console.log(answersBtnCon);
+console.log(allAnswers);
 
 //? Audio DOM
 const btnClick = new Audio("audio/button.wav");
@@ -127,18 +142,54 @@ const swoopIn = new Audio("audio/transition_up.wav");
 
 //? Take QUIZ DOM
 
-//! functions
+//! FUNCTIONS
+
 const displayNone = function () {
   title.style.display = "none";
   createQuizBTN.style.display = "none";
   takeQuizBTN.style.display = "none";
 };
 
+const addCurrentQuestion = function (data) {
+  currentQuestion = data;
+  correctAnswer = currentQuestion.correctAnswer;
+  nextQuestion = currentQuestion.nextQuestion;
+};
+
+const createStartQuestion = function () {
+  (document.querySelector(".start__title").innerHTML = `
+        <h1>
+          ${currentQuestion.question}
+        </h1>
+        `),
+    (document.querySelector(
+      ".a"
+    ).innerHTML = `<span>A: </span> ${currentQuestion.answers[0]}`),
+    (document.querySelector(
+      ".b"
+    ).innerHTML = `<span>B: </span> ${currentQuestion.answers[1]}`),
+    (document.querySelector(
+      ".c"
+    ).innerHTML = `<span>C: </span> ${currentQuestion.answers[2]}`),
+    (document.querySelector(
+      ".d"
+    ).innerHTML = `<span>D: </span> ${currentQuestion.answers[3]}`);
+};
+
+const addActive = function (clicked) {
+  allAnswers.forEach(function (e) {
+    e.classList.remove("active");
+  });
+  clicked.classList.add("active");
+};
+
 //! event listeners
+
+//? LANDIN PAGE BUTTONS
 
 buttons.addEventListener("click", function (e) {
   if (e.target.classList.contains("buttons__btn")) {
-    let nextPage = e.target.getAttribute("data-page");
+    nextPage = e.target.getAttribute("data-page");
     const nextBackground = e.target.getAttribute("background");
     console.log(nextPage);
     title.classList.add("title-escape");
@@ -157,6 +208,56 @@ buttons.addEventListener("click", function (e) {
     setTimeout(function () {
       swoopIn.play();
     }, 1200);
+
+    return nextPage;
   }
-  //   title.style.backgroundColor = "red";
 });
+
+//? CHOOSE QUIZ BUTTONS
+
+let s;
+
+chooseQuiz.forEach(function (e) {
+  e.addEventListener("click", function () {
+    nextPage.classList.add("title-escape");
+    const currentQuiz = e.getAttribute("data-set");
+    if (currentQuiz === "codingQuestion1") {
+      addCurrentQuestion(codingQuestion1);
+    } else if (currentQuiz === "geographyQuestion1") {
+      console.log("geographyQuestion1");
+    } else if (currentQuiz === "worldCupQuestion1") {
+      console.log("worldCupQuestion1");
+    }
+    setTimeout(function () {
+      nextPage.style.display = "none";
+    }, 1500);
+    setTimeout(function () {
+      body.classList.add("start-quiz-bg");
+    }, 1000);
+
+    setTimeout(function () {
+      startQuiz.style.display = "flex";
+    }, 1500);
+
+    createStartQuestion();
+
+    return currentQuestion, correctAnswer, nextQuestion, s;
+  });
+
+  // return currentQuestion, correctAnswer, nextQuestion;
+});
+
+answersBtnCon.addEventListener("click", function (e) {
+  if (e.target.classList.contains("start__answers")) return;
+  userChosenAnswer = e.target.getAttribute("data-set");
+  addActive(e.target);
+  console.log(userChosenAnswer);
+  return userChosenAnswer;
+});
+
+// allAnswers.forEach(function (e) {
+
+//   e.addEventListener("click", function () {
+//     console.log(e.target);
+//   });
+// });
